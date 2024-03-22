21667aeb14256754f88d11a8e5d6cf8213a77e41
Ryan Sanchez <ryansan@udel.edu>
2024-03-22 16:52:21 -0400

    WASD movement and arrow keys

--- src/scenes/levelAScene.ts
+++ src/scenes/levelAScene.ts
@@ -3,6 +3,12 @@ import Phaser from "phaser";
 export default class LevelAScene extends Phaser.Scene {
     private player?: Phaser.Physics.Arcade.Sprite;
     private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
+    private wasd?: {
+    W: Phaser.Input.Keyboard.Key,
+    A: Phaser.Input.Keyboard.Key,
+    S: Phaser.Input.Keyboard.Key,
+    D: Phaser.Input.Keyboard.Key
+};
 
     constructor() {
         super({ key: "LevelA"});
@@ -40,28 +46,38 @@ export default class LevelAScene extends Phaser.Scene {
         })
 
         this.cursors = this.input.keyboard?.createCursorKeys();
-
+        this.wasd = this.input.keyboard?.addKeys({
+            W: Phaser.Input.Keyboard.KeyCodes.W,
+            A: Phaser.Input.Keyboard.KeyCodes.A,
+            S: Phaser.Input.Keyboard.KeyCodes.S,
+            D: Phaser.Input.Keyboard.KeyCodes.D
+        }) as {
+            W: Phaser.Input.Keyboard.Key,
+            A: Phaser.Input.Keyboard.Key,
+            S: Phaser.Input.Keyboard.Key,
+            D: Phaser.Input.Keyboard.Key
+        };
     }
 
     update(){
-         if (!this.cursors) {
+         if (!this.cursors && !this.wasd) {
             return
         }
-        if (this.cursors.left.isDown) {
+        if (this.cursors?.left.isDown || this.wasd?.A.isDown) {
             this.player?.setVelocityX(-160);
             this.player?.anims.play("left", true);
         }
-        else if (this.cursors.right.isDown)
+        else if (this.cursors?.right.isDown || this.wasd?.D.isDown)
         {
             this.player?.setVelocityX(160);
             this.player?.anims.play("right", true);
         }
-        else if (this.cursors.up.isDown)
+        else if (this.cursors?.up.isDown || this.wasd?.W.isDown)
         {
             this.player?.setVelocityY(-160);
             this.player?.anims.play("turn", true);
         }
-        else if (this.cursors.down.isDown)
+        else if (this.cursors?.down.isDown || this.wasd?.S.isDown)
         {
             this.player?.setVelocityY(160);
             this.player?.anims.play("turn", true);
