a586559056e8ac8627792001e5fa0db5d8783574
Ryan Sanchez <ryansan@udel.edu>
2024-03-22 16:30:55 -0400

    level 1 setup

--- src/scenes/levelAScene.ts
+++ src/scenes/levelAScene.ts
@@ -1,17 +1,62 @@
 import Phaser from "phaser";
 
 export default class LevelAScene extends Phaser.Scene {
+    private player?: Phaser.Physics.Arcade.Sprite;
+    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
+
     constructor() {
         super({ key: "LevelA"});
 
     }
 
     create() {
-        this.add.text(165, 280, "Level A", {
-            fontSize: "90px",
-            color: "red",
+        this.add.image(640, 360, "backgroundA");
+        this.player = this.physics.add.sprite(160, 470, "wizard");
+        this.player.setScale(3);
+
+        // Wizard Movement: 
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 0, end: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: "turn",
+            frames: [{ key: "dude", frame: 4}],
+            frameRate: 20
+        })
+
+        this.anims.create({
+            key: "right", 
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 5, end: 8
+            }),
+            frameRate: 10,
+            repeat: -1
         })
     }
 
-    update(){}
+    update(){
+         if (!this.cursors) {
+            return
+        }
+        if (this.cursors.left.isDown) {
+            this.player?.setVelocityX(-160);
+            this.player?.anims.play("left", true);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player?.setVelocityX(160);
+            this.player?.anims.play("right", true);
+        }
+        else {
+            this.player?.setVelocityX(0);
+            this.player?.anims.play("turn");
+        }
+
+    }
 }
--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -8,6 +8,8 @@ export default class PreloadScene extends Phaser.Scene {
     preload() {
         this.load.image("startBackground", "assets/startBackground.png");
         this.load.image("startBtn", "assets/startBtn.png");
+        this.load.image("backgroundA", "assets/backgroundA.png");
+        this.load.spritesheet("wizard", "assets/wizard.png",  {frameWidth: 48, frameHeight: 52});
     }
 
     create() {
